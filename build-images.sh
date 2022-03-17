643a37d929f735553dbe4e1f9aa13bfe011fb176
stephdl <stephdl@de-labrusse.fr>
2022-03-17 16:39:23 +0000

    Repository initialization
    - Enable automated workflows
    - Replace "kickstart" occurrences with "newui"
    - Rename the service unit file

--- build-images.sh
+++ build-images.sh
@@ -8,19 +8,19 @@ images=()
 # The image will be pushed to GitHub container registry
 repobase="${REPOBASE:-ghcr.io/nethserver}"
 # Configure the image name
-reponame="kickstart"
+reponame="newui"
 
 # Create a new empty container image
 container=$(buildah from scratch)
 
-# Reuse existing nodebuilder-kickstart container, to speed up builds
-if ! buildah containers --format "{{.ContainerName}}" | grep -q nodebuilder-kickstart; then
+# Reuse existing nodebuilder-newui container, to speed up builds
+if ! buildah containers --format "{{.ContainerName}}" | grep -q nodebuilder-newui; then
     echo "Pulling NodeJS runtime..."
-    buildah from --name nodebuilder-kickstart -v "${PWD}:/usr/src:Z" docker.io/library/node:lts
+    buildah from --name nodebuilder-newui -v "${PWD}:/usr/src:Z" docker.io/library/node:lts
 fi
 
 echo "Build static UI files with node..."
-buildah run nodebuilder-kickstart sh -c "cd /usr/src/ui && yarn install && yarn build"
+buildah run nodebuilder-newui sh -c "cd /usr/src/ui && yarn install && yarn build"
 
 # Add imageroot directory to the container image
 buildah add "${container}" imageroot /imageroot
